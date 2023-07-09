c46a23d13059b83cec91298304c282e15f305919
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-07-09 14:01:53 +0530

    resolve db & dependancy issues

--- src/main/java/com/crudStore/store/repository/CustomerRepository.java
+++ src/main/java/com/crudStore/store/repository/CustomerRepository.java
@@ -3,5 +3,6 @@ package com.crudStore.store.repository;
 import com.crudStore.store.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
--- src/main/java/com/crudStore/store/service/CustomerServiceImpl.java
+++ src/main/java/com/crudStore/store/service/CustomerServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class CustomerServiceImpl implements CustomerService {
 
 
-    private final CustomerRepository customerRepo;
+    private CustomerRepository customerRepo;
 
 
     @Autowired
